692f82d1be649b3316053fd750689b898db4be24
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-05-11 21:29:31 +0300

    Frontend created.

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/components/root/App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
