e5c9af339cb38f20207b05fc967680134bce0123
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-05-12 15:44:18 +0300

    Edited parts added to the project.

--- frontend/src/components/root/App.jsx
+++ frontend/src/components/root/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Navi from "../../shared/navi/Navi";
+import CategoryContainer from "../category/CategoryContainer";
+import ProductContainer from "../product/ProductContainer";
+import NotFound from "../../shared/notfound/NotFound";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h3>Hello !</h3>
+        <Navi />
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/category" component={CategoryContainer}></Route>
+          <Route path="/product" component={ProductContainer}></Route>
+          <Route component={NotFound}></Route>
+        </Switch>
       </div>
     );
   }
